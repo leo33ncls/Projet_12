5d76c8a5d83f85488c44094bfcf9d541af7a0451
Léo Nicolas <leoncls3398@gmail.com>
2020-01-20 19:10:11 +0100

    Project creation

---
+++ Projet_12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Projet_12
+//
+//  Created by Léo NICOLAS on 20/01/2020.
+//  Copyright © 2020 Léo NICOLAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
