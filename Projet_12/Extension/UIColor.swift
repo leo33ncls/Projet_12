10075a7b0141bac44b99a747ee6d9e39460c4457
Léo Nicolas <leoncls3398@gmail.com>
2020-03-02 22:04:31 +0100

    Adding constraints

---
+++ Projet_12/Extension/UIColor.swift
@@ -0,0 +1,9 @@
+//
+//  UIColor.swift
+//  Projet_12
+//
+//  Created by Léo NICOLAS on 02/03/2020.
+//  Copyright © 2020 Léo NICOLAS. All rights reserved.
+//
+
+import Foundation
---
+++ Projet_12/View/CustomShadowLayer.swift
@@ -0,0 +1,13 @@
+//
+//  CustomShadowLayer.swift
+//  Projet_12
+//
+//  Created by Léo NICOLAS on 02/03/2020.
+//  Copyright © 2020 Léo NICOLAS. All rights reserved.
+//
+
+import UIKit
+
+class CustomShadowLayer: CAGradientLayer {
+
+}
