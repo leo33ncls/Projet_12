6c1cd048add6bc08db0f74b4c23242b89cfdd023
Léo Nicolas <leoncls3398@gmail.com>
2020-03-29 18:25:47 +0200

    APIKeys

---
+++ Projet_12/Supporting Files/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  Projet_12
+//
+//  Created by Léo NICOLAS on 11/02/2020.
+//  Copyright © 2020 Léo NICOLAS. All rights reserved.
+//
+
+import Foundation
+
+class APIKeys {
+    static let serieAPIKey = "clé"
+}
